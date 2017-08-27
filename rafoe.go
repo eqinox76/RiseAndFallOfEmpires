9320f72d2fef692f5d0f073df5fd4ab829238232
carsten daenschenl <carsten@daenschel.net>
2017-08-27 19:53:09 +0200

    added server port to send game state to clients

--- rafoe.go
+++ rafoe.go
@@ -4,16 +4,102 @@ import (
 	"fmt"
 	"github.com/eqinox76/RiseAndFallOfEmpires/state"
 	"github.com/eqinox76/RiseAndFallOfEmpires/engine"
+	"net"
+	"os"
+	"sync"
+	"encoding/binary"
+	"time"
 )
+
+type Worker struct {
+	input chan []byte
+	Done  bool
+}
 
 func main() {
 
 	space := state.NewSpace()
+	fanOut := make(chan []byte)
+	defer close(fanOut)
 
-	for i:=0 ; i< 8 ; i++ {
+	go func() {
+		workers := make([]*Worker, 0)
+		mutex := sync.Mutex{}
+
+		go func() {
+			// Listen for incoming connections.
+			l, err := net.Listen("tcp", "localhost:9076")
+			if err != nil {
+				fmt.Println("Error listening:", err.Error())
+				os.Exit(1)
+			}
+
+			defer l.Close()
+
+			fmt.Println("Listening on localhost:9076")
+			for {
+				// Listen for an incoming connection.
+				con, err := l.Accept()
+				if err != nil {
+					fmt.Println("Error accepting: ", err.Error())
+					os.Exit(1)
+				}
+
+				mutex.Lock()
+
+				w := Worker{input: make(chan []byte)}
+				workers = append(workers, &w)
+
+				mutex.Unlock()
+				go send(con, &w)
+			}
+		}()
+
+		for msg := range fanOut {
+			mutex.Lock()
+			for i, w := range workers {
+				fmt.Print(w)
+				if w.Done {
+					// this worker is done so remove it
+
+					close(w.input)
+					workers[i] = workers[ len(workers)-1]
+					workers = workers[:len(workers)-1]
+				} else {
+					w.input <- msg
+				}
+			}
+			mutex.Unlock()
+		}
+
+	}()
+
+	for true {
 		bytes, _ := space.Serialize()
 		fmt.Printf("serialize: %d, Planets: %d Ships: %d\n", len(bytes), len(space.Planets), len(space.Ships))
 		engine.Step(&space)
+		time.Sleep(1 * time.Second)
+		fanOut <- bytes
 	}
 
+}
+
+func send(con net.Conn, worker *Worker) {
+	for msg := range worker.input {
+
+		length := make([]byte, 4)
+		binary.LittleEndian.PutUint32(length, uint32(len(msg)))
+		_, err := con.Write(length)
+
+		if err != nil{
+			break
+		}
+
+		_, err = con.Write(msg)
+
+		if err != nil{
+			break
+		}
+	}
+	worker.Done = true
 }
